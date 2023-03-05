c926a2b4815f948aae69dec494511d3db2ce6793
bekakad0303 <bekturkadyrkulov@icloud.com>
2023-03-05 12:25:40 +0600

    Вроде бы правильно но у меня не выходит в фигме  какое должно быть расстояние

--- Homework0103/ViewController.swift
+++ Homework0103/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var TextField1: UITextField!
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var Logo: UIImageView!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBAction func Login(_ sender: Any) {
+    }
+    
+    @IBAction func Login2(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        if(TextField == nil){
+             TextField.backgroundColor = .red
+        }else if (TextField1 == nil){
+             TextField1.backgroundColor = .red
+        }
+                    
     }
 
-
+   
 }
+
 
