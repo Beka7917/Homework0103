744b62675663365fb4c29f452a3c3498c1de8ef7
bekakad0303 <bekturkadyrkulov@icloud.com>
2023-03-05 12:00:14 +0600

    Initial Commit

---
+++ Homework0103/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework0103
+//
+//  Created by Бектур Кадыркулов on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
